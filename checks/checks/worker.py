9e1edcdbee222f27cc406fb8d883eaed5d53e9c6
Jon de Andres <jon@rollbar.com>
2021-03-14 23:43:38 +0100

    Run job every `period`
    For now I am taking a quite naive approach implementing the scheduler
    logic. Ideally this logic is not part of the worker itself but injected somehow.

--- checks/checks/worker.py
+++ checks/checks/worker.py
@@ -1,8 +1,12 @@
 import asyncio
+import time
 
 from checks.lib.job import Job
 from checks.lib.repository import Repository
 from checks.lib.writer import Writer
+
+
+_DEFAULT_PERIOD = 5
 
 
 class Worker:
@@ -14,7 +18,7 @@ class Worker:
     received Job. The collected result is used to call the Writer.
 
     """
-    def __init__(self, job: Job, repository: Repository, writer: Writer):
+    def __init__(self, job: Job, repository: Repository, writer: Writer, period: int = _DEFAULT_PERIOD):
         """
         Args:
           job: A Job that implements the logic to execute per received object
@@ -24,11 +28,25 @@ class Worker:
         self.__job = job
         self.__repository = repository
         self.__writer = writer
+        self.__last_run = 0
+        self.__period = period
+        self.__is_running = True
+
+    def stop(self):
+        self.__is_running = False
 
     async def run(self) -> None:
-        results = await asyncio.gather(*[
-                self.__job.run(obj)
-                for obj in self.__repository.get_objects()
-        ])
+        while self.__is_running:
+            if self.__should_run():
+                results = await asyncio.gather(*[
+                        self.__job.run(obj)
+                        for obj in self.__repository.get_objects()
+                ])
 
-        self.__writer.write(results)
+                self.__writer.write(results)
+                self.__last_run = time.time()
+            else:
+                time.sleep(1)
+
+    def __should_run(self) -> bool:
+        return self.__last_run < (time.time() - self.__period)
