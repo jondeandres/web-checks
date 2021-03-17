b8a1262ad2a488dd0296b97f15fd3ddabccfb990
Jon de Andres <jon@rollbar.com>
2021-03-17 01:33:34 +0100

    Reorganize checker/bootstrap

--- checks/checks/bootstrap.py
+++ checks/checks/bootstrap.py
@@ -13,26 +13,32 @@ from checks.jobs.http_check import HTTPCheck
 def bootstrap() -> None:
     args = _parse_args()
 
+    _run_worker(args)
+
+
+def _run_worker(args: argparse.Namespace):
     worker = Worker(HTTPCheck(),
                     HardCoded(),
-                    KafkaWriter(_build_producer(args), args.topic, ResultToJSON()))
+                    KafkaWriter(_build_producer(args), args.kafka_topic, ResultToJSON()))
 
     asyncio.run(worker.run())
 
 
-def _build_producer(args: argparse.Namespace) -> ConfluentProducer:
-    return ConfluentProducer(KafkaConfig(brokers=args.brokers,
-                                         ssl_key_path=args.ssl_key_path,
-                                         ssl_certificate_path=args.ssl_cert_path,
-                                         ssl_ca_path=args.ssl_ca_path))
-
-
 def _parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
-    parser.add_argument("--brokers", required=True, nargs='+')
-    parser.add_argument("--topic", required=True)
-    parser.add_argument("--ssl-key-path")
-    parser.add_argument("--ssl-cert-path")
-    parser.add_argument("--ssl-ca-path")
+    parser.add_argument("--kafka-brokers", required=True, nargs='+')
+    parser.add_argument("--kafka-topic", required=True)
+    parser.add_argument("--kafka-ssl-key-path")
+    parser.add_argument("--kafka-ssl-cert-path")
+    parser.add_argument("--kafka-ssl-ca-path")
 
     return parser.parse_args()
+
+
+
+def _build_producer(args: argparse.Namespace) -> ConfluentProducer:
+    return ConfluentProducer(KafkaConfig(brokers=args.kafka_brokers,
+                                         ssl_key_path=args.kafka_ssl_key_path,
+                                         ssl_certificate_path=args.kafka_ssl_cert_path,
+                                         ssl_ca_path=args.kafka_ssl_ca_path))
+
